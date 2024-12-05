7812fdc479d0eeaa0d4bfabbc43364d0e9ddd358
Shifat507 <ferdoushasanshifat@gmail.com>
2024-12-05 19:45:29 +0600

    send all data to localhost

--- index.js
+++ index.js
@@ -15,31 +15,53 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const movieCollectionDB = client.db("movieDB").collection("movie");
+
+        //Read data / send all data to the localhost (API) from DB
+        app.get('/movies', async (req, res) => {
+            const cursor = movieCollectionDB.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // create 
+        app.post('/movies', async (req, res) => {
+            const newMovies = req.body;
+            console.log(newMovies);
+            const result = await movieCollectionDB.insertOne(newMovies);
+            res.send(result);
+        })
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send(`Movie server is running`);
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Movie server is running on port ${port}`);
 })
